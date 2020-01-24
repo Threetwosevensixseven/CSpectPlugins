4a42f26548e1574c890ba32b0322774e0c59fc6a
Threetwosevensixseven <robin.guest@gmail.com>
2020-01-23 20:33:28 -0500

    UART replacement now changes baud by listening to writes to the Next UART I/O ports. Renamed project to UARTReplacement.

---
+++ UARTReplacement/SerialPort.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plugins.UARTReplacement
+{
+    public class SerialPort : IDisposable
+    {
+        private System.IO.Ports.SerialPort port;
+        private int clock = 27000000; // CSpect defaults to HDMI timings
+        private int prescaler = 234;  // Next baud defaults to 115200 (more accurately 115384 with integer division)
+
+        public SerialPort(string PortName, int BaudRate)
+        {
+            try
+            {
+                port = new System.IO.Ports.SerialPort();
+                port.PortName = PortName;
+                int baud = Baud;
+                port.BaudRate = baud > 0 ? baud : 115200;
+                port.Parity = System.IO.Ports.Parity.None;
+                port.DataBits = 8;
+                port.StopBits = System.IO.Ports.StopBits.One;
+                port.Handshake = System.IO.Ports.Handshake.None;
+                LogClock(false);
+                LogPrescaler("");
+                port.Open();
+            }
+            catch (Exception /*ex*/)
+            {
+                port = null;
+            }
+        }
+
+        public void Write(byte[] buffer, int offset, int count)
+        {
+            if (port != null)
+                port.Write(buffer, offset, count);
+        }
+
+        /// <summary>
+        /// If there is a byte available in the UART buffer return it, otherwise a value representing no data.
+        /// </summary>
+        /// <param name="Success">Output parameter indicating whether the result was a valid byte or no data.</param>
+        /// <returns>A byte from the UART buffer, or a value representing no data.</returns>
+        public byte ReadByte(out bool Success)
+        {
+            try
+            {
+                if (port != null)
+                {
+                    var b = Convert.ToByte(port.ReadByte());
+                    Success = true;
+                    return b;
+                }
+            }
+            catch
+            {
+            }
+            Success = false;
+            return 0xff;
+        }
+
+        /// <summary>
+        /// Given a raw byte read from REG_VIDEO_TIMING, and another raw byte written to PORT_UART_CONTROL,
+        /// updates the clock and potentially also bits 17:13 of the prescaler.
+        /// </summary>
+        /// <param name="BaudByte">Read from REG_VIDEO_TIMING, used to update the clock.</param>
+        /// <param name="VideoTimingByte">Written to PORT_UART_CONTROL, used to update bits 17:13 of the prescaler.</param>
+        /// <returns></returns>
+        public int SetPrescalerAndClock(byte BaudByte, byte VideoTimingByte)
+        {
+            if (port == null)
+                return 0;
+            int mode = VideoTimingByte & 7;
+            switch (mode)
+            {
+                case 0:
+                    clock = 28000000;
+                    break;
+                case 1:
+                    clock = 28571429;
+                    break;
+                case 2:
+                    clock = 29464286;
+                    break;
+                case 3:
+                    clock = 30000000;
+                    break;
+                case 4:
+                    clock = 31000000;
+                    break;
+                case 5:
+                    clock = 32000000;
+                    break;
+                case 6:
+                    clock = 33000000;
+                    break;
+                case 7:
+                    clock = 27000000;
+                    break;
+                default:
+                    clock = 28000000;
+                    break;
+            }
+            if ((BaudByte & 0x10) == 0x10)
+            {
+                // Get bits 14..17 of the new prescaler
+                int newBits = (BaudByte & 0x07) << 14;
+                // Mask out everything of the existing prescaler except bits 14..17
+                int oldBits = prescaler & 0x3fff;
+                // Combine the two sets of bits
+                prescaler = oldBits | newBits;
+                port.BaudRate = Baud;
+                LogClock(false);
+                LogPrescaler("17:14");
+            }
+            else
+            {
+                port.BaudRate = Baud;
+                LogClock();
+            }
+            return clock;
+        }
+
+        /// <summary>
+        /// Given a raw byte written to PORT_UART_RX, parses the high bit to decide whether it represents a change
+        /// to bits 6:0 or 13:7, and updates the prescaler accordingly.
+        /// </summary>
+        /// <param name="BaudByte">The raw byte written to I/O port PORT_UART_RX.</param>
+        /// <returns>Returns the newly recalculated baud.</returns>
+        public int SetPrescaler(byte BaudByte)
+        {
+            if (port == null)
+                return 0;
+            if ((BaudByte & 0x80) == 0)
+            {
+                // Get bits 0..6 of the new prescaler
+                int newBits = BaudByte & 0x7f;
+                // Mask out everything of the existing prescaler except bits 0..6
+                int oldBits = prescaler & 0x1ff80;
+                // Combine the two sets of bits
+                prescaler = oldBits | newBits;
+                port.BaudRate = Baud;
+                LogPrescaler("6:0");
+            }
+            else
+            {
+                // Get bits 7..13 of the new prescaler
+                int newBits = (BaudByte & 0x7f) << 7;
+                // Mask out everything of the existing prescaler except bits 7..13
+                int oldBits = prescaler & 0x1c07f;
+                // Combine the two sets of bits
+                prescaler = oldBits | newBits;
+                port.BaudRate = Baud;
+                LogPrescaler("13:7");
+            }
+            return Baud;
+        }
+
+        /// <summary>
+        /// Baud is always calculated dynamically from the clock and prescaler, using integer division.
+        /// </summary>
+        public int Baud
+        {
+            get
+            {
+                // 
+                if (prescaler == 0)
+                    return 0;
+                return Convert.ToInt32(Math.Truncate((Convert.ToDecimal(clock) / prescaler)));
+            }
+        }
+
+        /// <summary>
+        /// Convenience method to log the clock and calculated baud to the debug console, every time the video timing clock changes.
+        /// </summary>
+        /// <param name="LogBaud">
+        /// Optionally choose not to log the calculated baud, if you know the prescaler is about to be changed
+        /// and logged straight afterwareds.
+        /// </param>
+        private void LogClock(bool LogBaud = true)
+        {
+            if (port == null)
+                return;
+            Debug.WriteLine("Clock:     " + clock);
+            if (LogBaud)
+                Debug.WriteLine("Baud:      " + Baud);
+        }
+
+        /// <summary>
+        /// Convenience method to log the prescaler and calculated baud to the debug console, every time the prescaler changes.
+        /// </summary>
+        /// <param name="BitsChanged"></param>
+        private void LogPrescaler(string BitsChanged)
+        {
+            if (port == null)
+                return;
+            string bstr = Convert.ToString(prescaler, 2).PadLeft(17, '0');
+            Debug.WriteLine("Prescaler: " + bstr.Substring(0, 3) + " " + bstr.Substring(3, 7) + " " + bstr.Substring(10, 7)
+                + " (" + prescaler + (string.IsNullOrWhiteSpace(BitsChanged) ? "" : ", changed bits " + BitsChanged) + ")");
+            Debug.WriteLine("Baud:      " + Baud);
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // Dispose managed state (managed objects).
+                    if (port != null)
+                    {
+                        if (port.IsOpen)
+                            port.Close();
+                        port.Dispose();
+                        port = null;
+                    }
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // TODO: set large fields to null.
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+        // ~SerialPort() {
+        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        //   Dispose(false);
+        // }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+}
---
+++ UARTReplacement/UARTReplacement_Device.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Plugin;
+
+namespace Plugins.UARTReplacement
+{
+    public class UARTReplacement_Device : iPlugin
+    {
+        private const ushort PORT_UART_TX = 0x133b;
+        private const ushort PORT_UART_RX = 0x143b;
+        private const ushort PORT_UART_CONTROL = 0x153b;
+        private const byte REG_VIDEO_TIMING = 0x11;
+
+
+        private iCSpect CSpect;
+        private UARTTargets Target;
+        private SerialPort espPort;
+        private bool UART_RX_Internal;
+        private bool UART_TX_Internal;
+        private Thread espThread;
+        private bool threadCancel;
+        private Queue<byte> readBuffer;
+        private object sync;
+
+        public List<sIO> Init(iCSpect _CSpect)
+        {
+            // Initialise and load plugin settings
+            readBuffer = new Queue<byte>();
+            sync = new object();
+            CSpect = _CSpect;
+            Target = UARTTargets.ESP;
+            espPort = new SerialPort("COM5", 115200);
+            UART_RX_Internal = false;
+            UART_TX_Internal = false;
+            threadCancel = false;
+            espThread = new Thread(ESPWork);
+            espThread.IsBackground = true;
+            espThread.Start();
+
+            // create a list of the ports we're interested in, but only if we're logging
+            List<sIO> ports = new List<sIO>();
+            ports.Add(new sIO(PORT_UART_TX, eAccess.Port_Write));
+            ports.Add(new sIO(PORT_UART_TX, eAccess.Port_Read));
+            ports.Add(new sIO(PORT_UART_RX, eAccess.Port_Write));
+            ports.Add(new sIO(PORT_UART_RX, eAccess.Port_Read));
+            ports.Add(new sIO(PORT_UART_CONTROL, eAccess.Port_Write));
+            return ports;
+        }
+
+        public void Quit()
+        {
+            threadCancel = true;
+            if (espPort != null)
+            {
+                espPort.Dispose();
+                espPort = null;
+            }
+        }
+
+        public bool Write(eAccess _type, int _port, byte _value)
+        {
+            switch (_port)
+            {
+                case PORT_UART_CONTROL:
+                    // Writes to this port contain siwtches between the ESP and Pi UARTs (we only handle the former),  
+                    // and also potential changes to the prescaler which cause the UART baud to change.
+                    // We might not need to query REG_VIDEO_TIMING every single time the baud or UART selector changes,
+                    // But just in case it can vary, let's do it. It doesn't seem to cause performance issues to do so.
+                    var target = (_value & 64) == 0 ? UARTTargets.ESP : UARTTargets.Pi;
+                    Target = target;
+                    if (Target == UARTTargets.ESP)
+                        espPort.SetPrescalerAndClock(_value, CSpect.GetNextRegister(REG_VIDEO_TIMING));
+                    // We are transparently logging without handling the write, so return false
+                    return false;
+                case PORT_UART_TX:
+                    if (Target == UARTTargets.ESP)
+                    {
+                        // This is an outgoing UART byte which should be sent to our buffered UART implementation
+                        espPort.Write(new byte[] { _value }, 0, 1);
+                        // If we are actively handling the write, return true.
+                        return true;
+                    }
+                    // If we are not handling the write, return false so CSpect's own Pi UART will handle it.
+                    return false;
+                case PORT_UART_RX:
+                    if (Target == UARTTargets.ESP)
+                    {
+                        // Writes to this port represent changes to the prescaler which cause the UART baud to change
+                        espPort.SetPrescaler(_value);
+                        return true;
+                    }
+                    return false;
+            }
+            // Don't handle any writes we didn't register for
+            return false;
+        }
+
+        public byte Read(eAccess _type, int _port, out bool _isvalid)
+        {
+            switch (_port)
+            {
+                case PORT_UART_RX:
+                    if (UART_RX_Internal)
+                    {
+                        // If this callback was triggered by the CSpect.InPort read from our own plugin,
+                        // then return without handling the callback.
+                        _isvalid = false;
+                        return 0;
+                    }
+                    // Otherwise, the callback was triggered by another plugin or Z80 code running inside
+                    // the emulator, so tell our plugin not to response to the next read callback,
+                    // then read the actual value, then handle the callback with the read value.
+                    if (Target == UARTTargets.ESP)
+                    {
+                        // This is an incoming UART byte which should be read from our buffered UART implementation
+                        // Z80 code will only call this if the status flag indicates data is ready when reading PORT_UART_TX.
+                        _isvalid = true;
+                        if (readBuffer.Count > 0)
+                        {
+                            lock (sync)
+                            {
+                                return readBuffer.Dequeue();
+                            }
+                        }
+                        else
+                            return 0xff;
+                    }
+                    // If not already handled, any remaining reads from this port represent data reads
+                    // from the Pi UART. So pass this through to let CSpect handle it (which initiates
+                    // another recursive call to this method, which we handle above).
+                    UART_RX_Internal = true;
+                    byte val = CSpect.InPort(PORT_UART_RX);
+                    UART_RX_Internal = false;
+                    _isvalid = true;
+                    return val;
+                case PORT_UART_TX:
+                    if (UART_TX_Internal)
+                    {
+                        // If this callback was triggered by the CSpect.InPort read from our own plugin,
+                        // then return without handling the callback.
+                        _isvalid = false;
+                        return 0;
+                    }
+                    if (Target == UARTTargets.ESP)
+                    {
+                        // Reads from this port return a status flag in bit 0 indicating whether 
+                        // there is any data available to read from the UART buffer.
+                        _isvalid = true;
+                        if (readBuffer.Count > 0)
+                            return 1;
+                        else
+                            return 0;
+                    }
+                    // If not already handled, any remaining reads from this port represent data ready status flag
+                    // reads from the Pi UART. So pass this through to let CSpect handle it (which initiates
+                    // another recursive call to this method, which we handle above).
+                    UART_RX_Internal = true;
+                    byte val2 = CSpect.InPort(PORT_UART_TX);
+                    UART_RX_Internal = false;
+                    _isvalid = true;
+                    return val2;
+            }
+            // Don't handle any reads we didn't register for
+            _isvalid = false;
+            return 0xff;
+        }
+
+
+        // This method is runs on a background thread to continuously read all available bytes
+        // proactively from the plugin's serial port, and fill the UART read buffer with them.
+        // Access to the buffer from both threads is synchronised with a mutex via the lock() construct.
+        // This method It may not offer the best performance, but so far I have not needed to optimise it.
+        public void ESPWork()
+        {
+            while(true)
+            {
+                if (threadCancel)
+                    return;
+                bool read;
+                var b = espPort.ReadByte(out read);
+                if (read)
+                {
+                    lock(sync)
+                    {
+                        readBuffer.Enqueue(b);
+                    }
+                }
+                Thread.Sleep(1);
+            }
+        }
+    }
+}
+
